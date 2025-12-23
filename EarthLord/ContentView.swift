9d5e230368687b0a6032875445c5f84e0a555325
shangenyu <shangmoai@vip.qq.com>
2025-12-23 22:08:34 +0800

    Initial Commit

---
+++ EarthLord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EarthLord
+//
+//  Created by Zhuanz密码0000 on 2025/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EarthLord/EarthLordApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthLordApp.swift
+//  EarthLord
+//
+//  Created by Zhuanz密码0000 on 2025/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EarthLordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
